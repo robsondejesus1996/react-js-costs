3b65d4491ac1e2f2e456fd29260cb18a909eb864
robson <robson.jesus@unidavi.edu.br>
2022-12-16 10:18:56 -0300

    Estruturando o projeto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
 
+import Container from "./components/layout/Container";
 
 function App() {
   return (
-    <>
-    <h1>Costs</h1>
-    <Footer/>
-    </>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <footer>Footer</footer>
+    </Router>
   );
 }
 
