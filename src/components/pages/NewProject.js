bc58de97df3b488bf6be0c0da98d9142e3174432
robson <robson.jesus@unidavi.edu.br>
2022-12-19 16:08:41 -0300

    Implementação Mensagens do Sistema erros e sucesso

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -5,7 +5,9 @@ import ProjectForm from '../project/ProjectForm';
 import styles from './NewProject.module.css';
 
 function NewProject() {
-    const history = useNavigate()
+    //const history = useNavigate()
+    const navigate = useNavigate()
+
   
     function createPost(project) {
       // initialize cost and services
@@ -21,7 +23,8 @@ function NewProject() {
       })
         .then((resp) => resp.json())
         .then((data) => {
-          history('/projects', { message: 'Projeto criado com sucesso!' })
+          //history('/projects', { message: 'Projeto criado com sucesso!' })
+          navigate('/projects', { state: {message: 'Projeto criado com sucesso!'} })
         })
     }
 
