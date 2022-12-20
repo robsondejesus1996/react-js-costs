101be0fd1da6a1d8273d297e9a0998d3c8ea6348
robson <robson.jesus@unidavi.edu.br>
2022-12-20 09:30:28 -0300

    Teste

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,20 +1,59 @@
-import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
 import Message from "../layout/Message";
 
-function Projects(){
-    const location = useLocation()
-    let message = ''
-    if(location.state){
-        message = location.state.message
-    }
+import styles from "../pages/Projects.module.css";
+import ProjectCard from "../project/ProjectCard";
 
-    return (
-        <div>
-            <h1>Meus Projetos</h1>
-            {message && <Message type="success" msg={message} />}
+function Projects() {
+  const [projects, setProjects] = useState();
+
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <div className={styles.project_container}>
+        <div className={styles.title_container}>
+          <h1>Meus Projetos</h1>
+          <LinkButton to="/newproject" text="Criar Projeto" />
         </div>
-    )
+      </div>
+      {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+      {projects.length > 0 &&
+          projects.map((project) => <ProjectCard
+          id={project.id}
+          name={project.name}
+          budget={project.budget}
+          category={project.category.name}
+          key={project.id}
+          />)}
+          
+      </Container>
+    </div>
+  );
 }
 
 export default Projects;
